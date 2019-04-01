98b802b356f41c98f6661995124bd3b0efc6c206
Chris Nickerson <christophern@nait.ca>
2019-04-01 14:01:48 -0600

    Added AI in C++ code

---
+++ Section_05/TestingGrounds/Source/TestingGrounds/Private/ChooseNextWaypoint.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ChooseNextWaypoint.h"
+
+EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+    Super::ExecuteTask(OwnerComp, NodeMemory);
+
+    UE_LOG(LogTemp, Warning, TEXT("Hello!"));
+
+    return EBTNodeResult::Succeeded;
+}
