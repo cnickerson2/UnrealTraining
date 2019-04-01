b8a3a52d1cbfcc8fb94ea499cd0aa2605ae21e39
Chris Nickerson <christophern@nait.ca>
2019-04-01 15:43:41 -0600

    Getting the Index Key Name from the Blackboard component

--- Section_05/TestingGrounds/Source/TestingGrounds/Private/ChooseNextWaypoint.cpp
+++ Section_05/TestingGrounds/Source/TestingGrounds/Private/ChooseNextWaypoint.cpp
@@ -1,12 +1,17 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ChooseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
     Super::ExecuteTask(OwnerComp, NodeMemory);
 
-    UE_LOG(LogTemp, Warning, TEXT("Hello!"));
+    auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+
+    auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+
+    UE_LOG(LogTemp, Warning, TEXT("Waypoint Index: %i"), Index);
 
     return EBTNodeResult::Succeeded;
 }
--- Section_05/TestingGrounds/Source/TestingGrounds/Public/ChooseNextWaypoint.h
+++ Section_05/TestingGrounds/Source/TestingGrounds/Public/ChooseNextWaypoint.h
@@ -17,4 +17,9 @@ class TESTINGGROUNDS_API UChooseNextWaypoint : public UBTTaskNode
 
 private:
     virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+protected:
+    UPROPERTY(EditAnywhere, Category = "Blackboard")
+    struct FBlackboardKeySelector IndexKey;
+
 };
