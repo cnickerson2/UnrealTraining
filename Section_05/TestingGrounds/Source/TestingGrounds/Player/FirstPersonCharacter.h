01e7aeb714a6cf1a895b3070545aa24d440f8ad0
Chris Nickerson <christophern@nait.ca>
2019-04-10 10:16:57 -0600

    Gun is now a separate actor that spawns at runtime

--- Section_05/TestingGrounds/Source/TestingGrounds/Player/FirstPersonCharacter.h
+++ Section_05/TestingGrounds/Source/TestingGrounds/Player/FirstPersonCharacter.h
@@ -7,6 +7,7 @@
 #include "FirstPersonCharacter.generated.h"
 
 class UInputComponent;
+class AGun;
 
 UCLASS(config=Game)
 class AFirstPersonCharacter : public ACharacter
@@ -16,14 +17,6 @@ class AFirstPersonCharacter : public ACharacter
 	/** Pawn mesh: 1st person view (arms; seen only by self) */
 	UPROPERTY(VisibleDefaultsOnly, Category=Mesh)
 	class USkeletalMeshComponent* Mesh1P;
-
-	/** Gun mesh: 1st person view (seen only by self) */
-	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
-	class USkeletalMeshComponent* FP_Gun;
-
-	/** Location on gun mesh where projectiles should spawn. */
-	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
-	class USceneComponent* FP_MuzzleLocation;
 
 	/** Gun mesh: VR view (attached to the VR controller directly, no arm, just the actual gun) */
 	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
@@ -64,21 +57,13 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Gameplay)
 	FVector GunOffset;
 
-	/** Projectile class to spawn */
-	UPROPERTY(EditDefaultsOnly, Category=Projectile)
-	TSubclassOf<class ABallProjectile> ProjectileClass;
-
-	/** Sound to play each time we fire */
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Gameplay)
-	class USoundBase* FireSound;
-
-	/** AnimMontage to play each time we fire */
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
-	class UAnimMontage* FireAnimation;
-
-	/** Whether to use motion controller location for aiming. */
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
-	uint32 bUsingMotionControllers : 1;
+    /** Whether to use motion controller location for aiming. */
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+    uint32 bUsingMotionControllers : 1;
+    
+    /** Whether to use motion controller location for aiming. */
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Gameplay)
+    TSubclassOf<AGun> GunBlueprint;
 
 protected:
 	
@@ -131,6 +116,8 @@ protected:
 	 * @returns true if touch controls were enabled.
 	 */
 	bool EnableTouchscreenMovement(UInputComponent* InputComponent);
+
+    AGun* Gun;
 
 public:
 	/** Returns Mesh1P subobject **/
