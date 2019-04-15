66e496461e83e482ae279b1aff78f7344bcc6a52
Chris Nickerson <christophern@nait.ca>
2019-04-15 15:42:58 -0600

    Reparented Character Blueprint to C++

---
+++ Section_05/TestingGrounds/Source/TestingGrounds/Character/Mannequin.h
@@ -0,0 +1,49 @@
+// Chris Nickerson -- TestingGrounds -- Unreal C++ Development
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Mannequin.generated.h"
+
+class AGun;
+class USkeletalMeshComponent;
+class UCameraComponent;
+
+UCLASS()
+class TESTINGGROUNDS_API AMannequin : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AMannequin();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Setup")
+    TSubclassOf<AGun> GunBlueprint;
+
+    UFUNCTION(BlueprintCallable, Category = "Weapon")
+    void Fire();
+
+private:
+    // First Person Mesh
+    UPROPERTY(VisibleDefaultsOnly, Category = "Mesh")
+    USkeletalMeshComponent * Mesh1P;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+    UCameraComponent * FirstPersonCameraComponent;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon", meta = (AllowPrivateAccess = "true"))
+    AGun * Gun;
+};
