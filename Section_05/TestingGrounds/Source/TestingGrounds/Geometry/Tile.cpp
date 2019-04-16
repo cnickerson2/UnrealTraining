446ee6bed3ed59dae99aa995a2abc48cc6405de0
Chris Nickerson <christophern@nait.ca>
2019-04-16 14:00:33 -0600

    Tile now spawns props at random

---
+++ Section_05/TestingGrounds/Source/TestingGrounds/Geometry/Tile.cpp
@@ -0,0 +1,47 @@
+// Chris Nickerson -- TestingGrounds -- Unreal C++ Development
+
+
+#include "Tile.h"
+
+// Sets default values
+ATile::ATile()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ATile::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ATile::PlaceActors(TSubclassOf<AActor> ToSpawn, int MinSpawn, int MaxSpawn)
+{
+    FVector Min (0, -2000, 0);
+    FVector Max (4000, 2000, 0);
+    FBox Bounds (Min, Max);
+    int NumberToSpawn = FMath::RandRange(MinSpawn, MaxSpawn);
+    for(size_t i=0; i < NumberToSpawn; i++)
+    {
+        FVector SpawnPoint = FMath::RandPointInBox(Bounds);
+        SpawnActorAtLocation(SpawnPoint, ToSpawn);
+    }
+}
+
+AActor* ATile::SpawnActorAtLocation(FVector SpawnPoint, TSubclassOf<AActor> ActorToSpawn)
+{
+    AActor* SpawnedActor = GetWorld()->SpawnActor<AActor>(ActorToSpawn);
+    SpawnedActor->SetActorRelativeLocation(SpawnPoint);
+    SpawnedActor->AttachToActor(this, FAttachmentTransformRules(EAttachmentRule::KeepRelative,false));
+    return SpawnedActor;
+}
---
+++ Section_05/TestingGrounds/Source/TestingGrounds/Geometry/Tile.h
@@ -0,0 +1,30 @@
+// Chris Nickerson -- TestingGrounds -- Unreal C++ Development
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Tile.generated.h"
+
+UCLASS()
+class TESTINGGROUNDS_API ATile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ATile();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+    UFUNCTION(BlueprintCallable, Category = "Spawning")
+    void PlaceActors(TSubclassOf<AActor> ToSpawn, int MinSpawn, int MaxSpawn);
+private:
+    AActor* SpawnActorAtLocation(FVector SpawnPoint, TSubclassOf<AActor> ActorToSpawn);
+};
